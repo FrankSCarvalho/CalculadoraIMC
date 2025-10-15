8180e39f1340491b96140b05a9da41f66356dd94
Frank Carvalho <frank.faculdadeadm@gmail.com>
2025-10-15 16:23:01 -0500

    commit

--- script.js
+++ script.js
@@ -5,7 +5,31 @@ function imc() {
     const nome = document.getElementById("nome");
     const altura = document.getElementById("altura");
     const peso = document.getElementById("peso");
-    const resultado = document.getElementById("resultado");   
+    const resultado = document.getElementById("resultado");
+
+    if (nome.value !== '' && !isNaN(parseFloat(altura.value)) && !isNaN(parseFloat(peso.value))) {
+        imc = parseFloat(peso.value) / (parseFloat(altura.value**2))
+        //imc = 30
+        
+        if(imc < 18.5){
+            resultado.innerText = `${nome.value}, seu IMC é: ${imc.toFixed(1)}. Você está abaixo do peso.`
+            return
+        }
+        if (imc >= 18.5 && imc <= 24.9) {
+            resultado.innerText = `${nome.value}, seu IMC é: ${imc.toFixed(1)}. Você está no peso ideal`
+            return
+        }
+        if (imc >= 25 && imc <= 29.9) {
+            resultado.innerText = `${nome.value}, seu IMC é: ${imc.toFixed(1)}. Você está com sobrepeso`
+            return
+        }
+        if (imc >= 30) {
+            resultado.innerText = `${nome.value}, seu IMC é: ${imc.toFixed(1)}. Você está obeso`
+            return
+        }
+    } else {
+        resultado.innerText = "Preencha todos os campos com valores validos!"
+    }
   
 }
 
